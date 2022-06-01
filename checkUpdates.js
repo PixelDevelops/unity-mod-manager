e25196c0cea8e722fb5c8c00c7ce28064159ccae
pixelxd <82414185+PixelDevelops@users.noreply.github.com>
2022-06-01 15:27:11 -0400

    Update checkUpdates.js

--- checkUpdates.js
+++ checkUpdates.js
@@ -9,7 +9,7 @@ let modList = JSON.parse(inputJSON);
 let result = []
 
 modList.mods.forEach(mod => 
-  https.get(`${prefix}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'PixelDevelops/unity-mod-manager' ,'Authorization': `Token ${process.env.SECRET}`}},(res) => {
+  https.get(`${prefix}${mod.gitPath}${postfix}`, { headers: { 'User-Agent' : 'PixelDevelops/unity-mod-manager' ,'Authorization': `Token ${process.env.UPDATE_MODS_TOKEN}`}},(res) => {
     let body = "";
 
       res.on("data", (chunk) => {
